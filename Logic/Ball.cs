c2928dc140d9ca59334ca299160cf81b50bcc1cf
Bartosz Podemski <bartekpodemski@gmail.com>
2024-04-02 13:27:22 +0200

    Implement ball class

--- Logic/Ball.cs
+++ Logic/Ball.cs
@@ -1,7 +1,41 @@
 ﻿namespace Logic
 {
-    public class Class1
+    public class Ball
     {
+        private int _x;
+        private int _y;
+        private int _radius;
+        
+        public Ball(int x, int y, int radius)
+        {
+            _x = x;
+            _y = y;
+            _radius = radius;
+        }
+
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public int Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public int Radius
+        {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+
+        public void Move(int dx, int dy)
+        {
+            _x += dx;
+            _y += dy;
+        }
 
     }
 }
