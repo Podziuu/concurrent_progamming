3b6dac3ead1004c604d6f05bc008da8961f2c2a6
Bartosz Podemski <bartekpodemski@gmail.com>
2024-04-02 13:50:41 +0200

    Add abstract API for logic and boilerplate for PoolTableManager

---
+++ Logic/LogicAbstractAPI.cs
@@ -0,0 +1,17 @@
+﻿namespace Logic
+{
+    public abstract class LogicAbstractAPI
+    {
+        public static LogicAbstractAPI CreateApi()
+        {
+            return new PoolTableManager();
+        }
+
+        public abstract void InitiatePoolTable(int width, int height, int ballsQuantity, int ballRadius);
+        public abstract void CreateBalls();
+        public abstract List<Ball> GetBalls();
+        public abstract bool IsActive();
+        public abstract void Activate();
+        public abstract void Deactivate();
+    }
+}
---
+++ Logic/PoolTableManager.cs
@@ -0,0 +1,37 @@
+﻿namespace Logic
+{
+    internal class PoolTableManager : LogicAbstractAPI
+    {
+        private PoolTable _poolTable;
+
+        public override void InitiatePoolTable(int width, int height, int ballsQuantity, int ballRadius)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateBalls()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Ball> GetBalls()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Activate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Deactivate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
